13b9919753a70fa4395676c8e7a2a32d5e729e9d
Faizal <lajabu.john@wonderlabz.com>
2021-12-26 22:35:45 +0200

    updated icon+fonts, added exceltofirebase, table structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,10 +10,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:weddingrsvp/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
 
-
-  });
 }
